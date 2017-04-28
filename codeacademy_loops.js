e8954bc8ccd65caaf71c5bab7f33c1223f46f354
Constantine <cdiam@windowslive.com>
2017-04-28 23:16:09 +0300

    adding loops

--- codeacademy_loops.js
+++ codeacademy_loops.js
@@ -65,3 +65,18 @@ for (var i = 0; i < myPlaces.length; i++) {
 				}
     }
 }
+
+
+var cards = ['Diamond', 'Spade', 'Heart', 'Club'];
+
+var currentCard = 'Heart';
+
+while (currentCard !== 'Spade') {
+  console.log(currentCard);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  currentCard = cards[randomNumber];
+}
+
+console.log('Found a Spade!');

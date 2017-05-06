cb52139b2ad9d9cb2a4faf2755227ea9df727520
Constantine <cdiam@windowslive.com>
2017-05-06 15:07:16 +0300

    Dragon slayer game

--- codeacademy_loops.js
+++ codeacademy_loops.js
@@ -241,3 +241,51 @@ var loopCondition = false;
 do {
 	console.log("I'm gonna stop looping 'cause my condition is " + loopCondition + "!");
 } while (loopCondition);
+
+
+
+
+/*
+
+Dragon Slayer game
+
+
+Declare some variables */
+
+var slaying = true;
+
+var youHit = Math.floor(Math.random() * 2);
+
+var damageThisRound = Math.floor(Math.random()*5 + 1);
+
+var totalDamage = 0 ;
+
+while ( slaying )
+{
+
+    if ( youHit === 1)
+    {
+        console.log ( "You hit the dragon and you won!!!");
+        totalDamage += damageThisRound;
+
+        if (totalDamage >= 4 )
+        {
+            slaying = false;
+            console.log(" You win !!!");
+
+        }
+        else
+        {
+            youHit = Math.floor(Math.random() * 2);
+        }
+
+    }
+
+    else
+    {
+        console.log (" The dragon defeated you!!");
+
+        slaying = false;
+    }
+
+}

495fe006e62eb2859f0f66fe5f8edf8c1fb2deac
Constantine <cdiam@windowslive.com>
2017-05-09 12:05:19 +0300

    adding switch code

--- ifelse_javascript.js
+++ ifelse_javascript.js
@@ -148,3 +148,21 @@ switch statements look like this:
              console.log('Invalid moon phase');
          	break;
          }
+
+
+         var color = prompt("What's your favorite primary color?","Type your favorite color here");
+
+         switch(color) {
+           case 'red':
+             console.log("Red's a good color!");
+             break;
+           case 'blue':
+             console.log("That's my favorite color, too!");
+             break;
+           //Add your case here!
+           case 'yellow':
+               console.log("This is AEK color");
+               break;
+           default:
+             console.log("I don't think that's a primary color!");
+         }

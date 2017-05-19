f3c8207b73bd871ead43a91319120d5f72003c62
Constantine <cdiam@windowslive.com>
2017-05-20 00:05:02 +0300

    more code for objects

--- objects.js
+++ objects.js
@@ -105,3 +105,65 @@ var bicycle = {
     frame_material : "carbon fiber"
 
 }
+
+
+
+// here is bob again, with his usual properties
+var bob = new Object();
+bob.name = "Bob Smith";
+bob.age = 30;
+// this time we have added a method, setAge
+bob.setAge = function (newAge){
+  bob.age = newAge;
+};
+// here we set bob's age to 40
+bob.setAge(40);
+// bob's feeling old.  Use our method to set bob's age to 20
+bob.setAge(20);
+
+
+
+// here we define our method using "this", before we even introduce bob
+var setAge = function (newAge) {
+  this.age = newAge;
+};
+// now we make bob
+var bob = new Object();
+bob.age = 30;
+bob.setAge = setAge;
+
+// make susan here, and first give her an age of 25
+
+var susan = {
+
+    age : 25,
+    setAge : setAge
+
+
+}
+
+
+// here, update Susan's age to 35 using the method
+susan.setAge(35);
+
+
+
+var rectangle = new Object();
+rectangle.height = 3;
+rectangle.width = 4;
+// here is our method to set the height
+rectangle.setHeight = function (newHeight) {
+  this.height = newHeight;
+
+};
+// help by finishing this method
+rectangle.setWidth = function (newWidth){
+    this.width = newWidth;
+}
+
+
+// here change the width to 8 and height to 6 using our new methods
+
+
+rectangle.setWidth(8);
+rectangle.setHeight(6);

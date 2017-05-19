ee7efcdacd4736a1dbfb42e234a0ce8b28f8bc6b
Constantine <cdiam@windowslive.com>
2017-05-19 20:17:17 +0300

    code for objects

--- objects.js
+++ objects.js
@@ -78,3 +78,30 @@ var search = function(friends) {
         }
     }
 };
+
+
+
+// help us make snoopy using literal notation
+// Remember snoopy is a "beagle" and is 10 years old.
+var snoopy = {
+
+    species: "beagle",
+    age: 10
+
+}
+
+// help make buddy using constructor notation
+// buddy is a "golden retriever" and is 5 years old
+var buddy = new Object();
+
+buddy.species = "golden retriever";
+buddy.age = 5;
+
+
+var bicycle = {
+
+    speed: 0,
+    gear: 1,
+    frame_material : "carbon fiber"
+
+}

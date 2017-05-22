4c30680a0e8c203a6af4ca848a44df60c524cd97
Constantine <cdiam@windowslive.com>
2017-05-22 11:08:33 +0300

    Object Constructor

--- objects.js
+++ objects.js
@@ -167,3 +167,25 @@ rectangle.setWidth = function (newWidth){
 
 rectangle.setWidth(8);
 rectangle.setHeight(6);
+
+
+
+/*
+So instead of using the Object constructor which is empty and has no properties, we can make our own constructors which have properties.
+
+To see how this works, look at our Person constructor in lines 1–4. This constructor is used to make Person objects.
+Notice it uses the keyword this to define the name and age properties and set them equal to the parameters given.
+
+*/
+
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Let's make bob and susan again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 25);
+// help us make george, whose name is "George Washington" and age is 275
+var george = new Person("George Washigton", 275);

1f3199b3c3466a195385f74d3f9345672ac42449
Constantine <cdiam@windowslive.com>
2017-05-24 09:41:48 +0300

    adding more objects

--- objects.js
+++ objects.js
@@ -212,3 +212,43 @@ var sally = new Person("Sally Bowles", 39);
 var holden = new Person("Holden Caulfield", 16);
 console.log("sally's species is " + sally.species + " and she is " + sally.age );
 console.log("holden's species is " + holden.species + " and he is " + holden.age);
+
+
+
+function Rectangle(height, width) {
+  this.height = height;
+  this.width = width;
+  this.calcArea = function() {
+      return this.height * this.width;
+  };
+  // put our perimeter function here!
+  this.calcPerimeter = function(){
+      return (this.height * 2) + (this.width * 2);
+  }
+}
+
+var rex = new Rectangle(7,3);
+var area = rex.calcArea();
+var perimeter = rex.calcPerimeter();
+
+
+
+
+
+
+
+function Rabbit(adjective) {
+    this.adjective = adjective;
+    this.describeMyself = function() {
+        console.log("I am a " + this.adjective + " rabbit");
+    };
+}
+
+// now we can easily make all of our rabbits
+var rabbit1 = new Rabbit("fluffy");
+var rabbit2 = new Rabbit("happy");
+var rabbit3 = new Rabbit("sleepy");
+
+rabbit1.describeMyself();
+rabbit2.describeMyself();
+rabbit3.describeMyself();

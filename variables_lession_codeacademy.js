78b2a634f8fbef8198d900746c686f86d4ef18d3
Constantine <cdiam@windowslive.com>
2017-08-26 14:09:18 +0300

    New file for new lesson in CodeAcademy

---
+++ variables_lession_codeacademy.js
@@ -0,0 +1,18 @@
+/*JavaScript creates space for this variable in memory and sets it to undefined.
+Undefined is the fifth and final primitive data type.
+JavaScript assigns the undefined data type to variables that are not assigned a value.*/
+
+let notDefined;
+console.log(notDefined);
+let valueless;
+console.log(valueless);
+
+
+let favoriteAnimal = 'dog';
+
+console.log('My favorite animal: ' + favoriteAnimal);
+
+
+let myName = 'Constantine';
+let myCity = 'Athens';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);

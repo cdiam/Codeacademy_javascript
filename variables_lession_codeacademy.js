e5005247e0a7ff640817fa96bc18b70ebb5810d3
Constantine <cdiam@windowslive.com>
2017-10-11 20:50:20 +0300

    More javascript code

--- variables_lession_codeacademy.js
+++ variables_lession_codeacademy.js
@@ -292,3 +292,55 @@ const myNightSky = () => {
 };
 
 console.log(myNightSky());
+
+
+
+
+const visibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+  let region = 'The Arctic';
+
+  if (region === 'The Arctic'){
+
+    let lightWaves = 'Northern Lights';
+
+      console.log(lightWaves);
+
+
+
+  };
+
+  console.log(lightWaves);
+};
+
+visibleLightWaves();
+
+
+
+const starCount = () => {
+  let i = 5 ;
+
+  console.log(i);
+
+  for ( let i = 0 ; i < 12 ; i++){
+
+    console.log(i);
+  }
+
+};
+
+starCount();
+
+console.log(i);
+
+
+
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+console.log(groceryList);
+
+groceryList.unshift('popcorn');
+console.log(groceryList);
+
+console.log(groceryList.slice(1,4));

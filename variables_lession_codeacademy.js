ea2c484843d68668cca8cac4a70302375a64d66a
Constantine <cdiam@windowslive.com>
2017-10-14 14:27:15 +0300

    iteration

--- variables_lession_codeacademy.js
+++ variables_lession_codeacademy.js
@@ -344,3 +344,101 @@ groceryList.unshift('popcorn');
 console.log(groceryList);
 
 console.log(groceryList.slice(1,4));
+
+
+
+//iterator Lesson
+
+
+let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+
+//Function method
+
+fruits.forEach(function(fruitsItem){
+
+  console.log('I want to eat ' + fruitsItem);
+
+});
+
+
+//Arrow function forEach
+fruits.forEach(fruitsItem=>
+console.log('I want to eat a ' + fruitsItem));
+
+
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+
+let secretMessage = animals.map(function(animals) {
+  return animals[0];
+
+})
+
+console.log(secretMessage.join(''));
+
+let bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+
+let smallNumbers = bigNumbers.map(function(bigNumbers){
+
+  return bigNumbers / 100;
+
+});
+
+let secretMessage = animals.map(animals => animals[0]);
+
+
+
+let randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+
+let smallNumbers = randomNumbers.filter(function(randomNumber){
+
+  return randomNumber < 250;
+});
+
+let favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+let longFavoriteWords = favoriteWords.filter(word =>
+  word.length > 7);
+
+// Refactor the code above using arrow function syntax
+
+
+let words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+// Something is missing in the method call below
+
+console.log(words.some(function(word) {
+  return word.length < 6;
+}));
+
+// Use filter to create a new array
+
+let interestingWords = words.filter(word => word.length > 5);
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+console.log(interestingWords.every(word =>  word.length > 5      ));
+
+
+let cities = ['Nashville', 'Charlotte', 'Asheville', 'Austin', 'Boulder'];
+
+let nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+let longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a new array
+let smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.some(num => num < 0);
